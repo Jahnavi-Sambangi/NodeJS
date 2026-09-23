466683237e98b6c818f795f727178348d681852f
Jahnavi Sambangi <25wh1a05l8@bvrithyderabad.edu.in>
2026-09-23 13:10:36 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,32 @@
+console.log("My name is Jahnavi");
+alert("Sambangi Jahnavi");
+
+console.log("My name is Jahnavi");
+j = "Jahnavi";
+console.log(j)
+
+var jahnavi = "typing";
+console.log(jahnavi)
+
+var jahnavi = "executing";
+console.log(jahnavi)
+
+let college = "BVRITH"
+console.log(college);
+
+college = "CSE"
+console.log(college);
+
+const age = 19;
+console.log(age);
+
+if (age>18) {
+    alert("Eligible to vote");
+}
+else 
+{
+    alert("Not eligible to vote");
+}
+
+document.write("Hello world");
+document.write("<h1>My name is Jahnavi</h1>");
